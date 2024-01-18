6c5b6ceae65887bbdf94105482ae69ba7b390661
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-01-18 13:39:40 +0100

    Update install_build_dep.sh

--- install_build_dep.sh
+++ install_build_dep.sh
@@ -6,4 +6,4 @@ sudo apt install -y gcc-arm* libc6-armel-cross libc6-dev-armel-cross binutils-ar
 sudo apt install -y liblz4-tool libgnutls28-dev libguestfs-tools libncurses-dev libpython3-dev libsdl2-dev libssl-dev lz4 lzma lzma-alone openssl
 sudo apt install -y pkg-config python3 python3-asteval python3-coverage python3-filelock python3-pkg-resources python3-pycryptodome python3-pyelftools 
 sudo apt install -y python3-pytest python3-pytest-xdist python3-sphinxcontrib.apidoc python3-sphinx-rtd-theme python3-subunit python3-testtools
-sudo apt install -y python3-virtualenv swig uuid-dev
+sudo apt install -y python3-virtualenv swig3.0 uuid-dev
