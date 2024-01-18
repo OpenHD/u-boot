64133c410af689814f8ac7efb4d78dd35cbf7f56
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-01-18 23:55:04 +0100

    Update install_build_dep.sh

--- install_build_dep.sh
+++ install_build_dep.sh
@@ -5,7 +5,7 @@ echo "installing deps"
 sudo apt update
 ls  /etc/apt/sources.list.d/
 cat /etc/apt/sources.list
-apt search swig | grep swig
+apt search swig
 
 sudo apt install -y gcc-arm* libc6-armel-cross libc6-dev-armel-cross binutils-arm-linux-gnueabi libncurses5-dev build-essential bison flex libssl-dev bc gcc gcc-arm-none-eabi bc bison build-essential coccinelle device-tree-compiler dfu-util efitools flex gdisk graphviz imagemagick
 sudo apt install -y automake liblz4-tool libgnutls28-dev libguestfs-tools libncurses-dev libpython3-dev libsdl2-dev libssl-dev lz4 lzma lzma-alone openssl
